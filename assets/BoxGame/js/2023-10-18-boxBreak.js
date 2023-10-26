1e200ebdcd96bd8dd3c10557282aa3ebe01d95dc
vs26625 <varnikasubramani@gmail.com>
2023-10-26 07:59:57 -0700

    transfering to js and html

--- assets/BoxGame/js/2023-10-18-boxBreak.js
+++ assets/BoxGame/js/2023-10-18-boxBreak.js
@@ -1,109 +1,93 @@
-<body>
-    <div>
-        <canvas id="spriteContainer"> <!-- Within the base div is a canvas. An HTML canvas is used only for graphics. It allows the user to access some basic functions related to the image created on the canvas (including animation) -->
-            <img id="box" src="{{site.baseurl}}/images/boxBreak.png"> 
-        </canvas>
-        <div id="controls"> <!--basic radio buttons which can be used to check whether each individual animaiton works -->
-            <input type="radio" name="animation" id="default" checked>
-            <label for="default">default</label><br>
-        </div>
-    </div>
-</body>
+window.addEventListener('load', function () {
+    const canvas = document.getElementById('spriteContainer');
+    const ctx = canvas.getContext('2d');
+    const SPRITE_WIDTH = 102.3; // Matches sprite pixel width
+    const SPRITE_HEIGHT = 115; // Matches sprite pixel height
+    const FRAME_LIMIT = 5; // Number of frames per sprite row
+    const SCALE_FACTOR = 3; // Control size of sprite on canvas
+    const DESIRED_FRAME_RATE = 4; // Frames per second
+    const FRAME_INTERVAL = 1000 / DESIRED_FRAME_RATE;
 
-<script>
-    // start on page load
-    window.addEventListener('load', function () {
-        const canvas = document.getElementById('spriteContainer');
-        const ctx = canvas.getContext('2d');
-        const SPRITE_WIDTH = 102.3;  // matches sprite pixel width
-        const SPRITE_HEIGHT = 115; // matches sprite pixel height
-        const FRAME_LIMIT = 5; // matches number of frames per sprite row, this code assume each row is same
-       // const FRAME_RATE = 4;  // Change this value to adjust the frame rate (frames per second)
-        const SCALE_FACTOR = 3;  // control size of sprite on canvas
-        const DESIRED_FRAME_RATE = 4; // 1 frames per second
-        const FRAME_INTERVAL = 1000 / DESIRED_FRAME_RATE;
+    canvas.width = SPRITE_WIDTH * SCALE_FACTOR;
+    canvas.height = SPRITE_HEIGHT * SCALE_FACTOR;
 
-        canvas.width = SPRITE_WIDTH * SCALE_FACTOR;
-        canvas.height = SPRITE_HEIGHT * SCALE_FACTOR;
-
-        class Cat {
-            constructor() {
-                this.image = document.getElementById("box");
-                this.x = 0;
-                this.y = 0;
-                this.minFrame = 0;
-                this.frameY = 0;
-                this.frameX = 0;
-                this.maxFrame = FRAME_LIMIT;
-                this.frameX = 0;
-                this.frameY = 0;
-            }
-
-            // draw object
-            draw(context) {
-                context.drawImage(
-                    this.image,
-                    this.frameX * SPRITE_WIDTH,
-                    this.frameY * SPRITE_HEIGHT,
-                    SPRITE_WIDTH,
-                    SPRITE_HEIGHT,
-                    this.x,
-                    this.y,
-                    canvas.width,
-                    canvas.height
-                );
-            }
-
-            // update frameX of object
-            update() {
-                if (this.frameX < this.maxFrame) {
-                    this.frameX++;
-                } else {
-                    this.frameX = 0;
-                }
-            }
+    class Cat {
+        constructor() {
+            this.image = document.getElementById("box");
+            this.x = 0;
+            this.y = 0;
+            this.minFrame = 0;
+            this.frameY = 0;
+            this.frameX = 0;
+            this.maxFrame = FRAME_LIMIT;
+            this.frameX = 0;
+            this.frameY = 0;
         }
 
-        // object
-        const cat = new Cat();
-
-        // update frameY of object, action from idle, bark, walk radio control
-        const controls = document.getElementById('controls');
-        controls.addEventListener('click', function (event) {
-            if (event.target.tagName === 'INPUT') {
-                const selectedAnimation = event.target.id;
-                switch (selectedAnimation) {
-                    case 'defult':
-                        cat.frameY = 5;
-                        break;
-                }
-            }
-        });
-
-        let lastTimestamp = 0;
-
-        // Animation recursive control function
-        function animate(timestamp) {
-            const deltaTime = timestamp - lastTimestamp;
-            if (deltaTime >= FRAME_INTERVAL) {
-                // Clears the canvas to remove the previous frame.
-                ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-                // Draws the current frame of the sprite.
-                cat.draw(ctx);
-
-                // Updates the `frameX` property to prepare for the next frame in the sprite sheet.
-                cat.update();
-
-                lastTimestamp = timestamp;
-                }
-        
-                // Uses `requestAnimationFrame` to synchronize the animation loop with the display's refresh rate,
-            // ensuring smooth visuals.
-            requestAnimationFrame(animate);
+        // Draw object
+        draw(context) {
+            context.drawImage(
+                this.image,
+                this.frameX * SPRITE_WIDTH,
+                this.frameY * SPRITE_HEIGHT,
+                SPRITE_WIDTH,
+                SPRITE_HEIGHT,
+                this.x,
+                this.y,
+                canvas.width,
+                canvas.height
+            );
         }
 
-        // run 1st animate
-        animate();
+        // Update frameX of object
+        update() {
+            if (this.frameX < this.maxFrame) {
+                this.frameX++;
+            } else {
+                this.frameX = 0;
+            }
+        }
+    }
+
+    // Object
+    const cat = new Cat();
+
+    // Update frameY of object, action from idle, bark, walk radio control
+    const controls = document.getElementById('controls');
+    controls.addEventListener('click', function (event) {
+        if (event.target.tagName === 'INPUT') {
+            const selectedAnimation = event.target.id;
+            switch (selectedAnimation) {
+                case 'defult':
+                    cat.frameY = 5;
+                    break;
+            }
+        }
     });
-</script>
+
+    let lastTimestamp = 0;
+
+    // Animation recursive control function
+    function animate(timestamp) {
+        const deltaTime = timestamp - lastTimestamp;
+        if (deltaTime >= FRAME_INTERVAL) {
+            // Clears the canvas to remove the previous frame.
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+            // Draws the current frame of the sprite.
+            cat.draw(ctx);
+
+            // Updates the `frameX` property to prepare for the next frame in the sprite sheet.
+            cat.update();
+
+            lastTimestamp = timestamp;
+        }
+
+        // Uses `requestAnimationFrame` to synchronize the animation loop with the display's refresh rate,
+        // ensuring smooth visuals.
+        requestAnimationFrame(animate);
+    }
+
+    // Run the animation
+    animate();
+});
--- assets/BoxGame/js/Platform_Animation.js
+++ assets/BoxGame/js/Platform_Animation.js
@@ -1,90 +1,84 @@
-<body>
-    <div>
-        <canvas id="spriteContainer"></canvas>
-    </div>
-</body>
+window.addEventListener('load', function () {
+    const canvas = document.getElementById('spriteContainer');
+    const ctx = canvas.getContext('2d');
+    const SPRITE_WIDTH = 362.25;
+    const SPRITE_HEIGHT = 377;
+    const SCALE_FACTOR = 0.25;
+    const FRAME_LIMIT = 4;
 
-<script>
-    window.addEventListener('load', function () {
-        const canvas = document.getElementById('spriteContainer');
-        const ctx = canvas.getContext('2d');
-        const SPRITE_WIDTH = 362.25;
-        const SPRITE_HEIGHT = 377;
-        const SCALE_FACTOR = 0.25;
-        const FRAME_LIMIT = 4;
+    canvas.width = SPRITE_WIDTH * SCALE_FACTOR;
+    canvas.height = SPRITE_HEIGHT * SCALE_FACTOR;
 
-        canvas.width = SPRITE_WIDTH * SCALE_FACTOR;
-        canvas.height = SPRITE_HEIGHT * SCALE_FACTOR;
+    const platformImage = new Image();
+    platformImage.src = "{{site.baseurl}}/images/platform.png";
 
-        const platformImage = new Image();
-        platformImage.src = "{{site.baseurl}}/images/platform.png";
-
-        platformImage.onload = function () {
-            class Platform {
-                constructor() {
-                    this.image = platformImage;
-                    this.spriteWidth = SPRITE_WIDTH;
-                    this.spriteHeight = SPRITE_HEIGHT;
-                    this.width = this.spriteWidth;
-                    this.height = this.spriteHeight;
-                    this.x = 0;
-                    this.y = 0;
-                    this.scale = SCALE_FACTOR;
-                    this.minFrame = 0;
-                    this.maxFrame = FRAME_LIMIT;
-                    this.frameX = 0;
-                    this.frameY = 0;
-                }
-
-                draw(context) {
-                    context.drawImage(
-                        this.image,
-                        this.frameX * this.spriteWidth,
-                        this.frameY * this.spriteHeight,
-                        this.spriteWidth,
-                        this.spriteHeight,
-                        this.x,
-                        this.y,
-                        this.width * this.scale,
-                        this.height * this.scale
-                    );
-                }
-
-                update() {
-                    if (this.frameX < this.maxFrame) {
-                        this.frameX++;
-                    } else {
-                        this.frameX = 0;
-                    }
-                }
+    platformImage.onload = function () {
+        class Platform {
+            constructor() {
+                this.image = platformImage;
+                this.spriteWidth = SPRITE_WIDTH;
+                this.spriteHeight = SPRITE_HEIGHT;
+                this.width = this.spriteWidth;
+                this.height = this.spriteHeight;
+                this.x = 0;
+                this.y = 0;
+                this.scale = SCALE_FACTOR;
+                this.minFrame = 0;
+                this.maxFrame = FRAME_LIMIT;
+                this.frameX = 0;
+                this.frameY = 0;
             }
 
-            const platform = new Platform();
+            draw(context) {
+                context.drawImage(
+                    this.image,
+                    this.frameX * this.spriteWidth,
+                    this.frameY * this.spriteHeight,
+                    this.spriteWidth,
+                    this.spriteHeight,
+                    this.x,
+                    this.y,
+                    this.width * this.scale,
+                    this.height * this.scale
+                );
+            }
 
-            let animationHasRun = false;
+            update() {
+                if (this.frameX < this.maxFrame) {
+                    this.frameX++;
+                } else {
+                    this.frameX = 0;
+                }
+            }
+        }
 
-            platform.draw(ctx);
-            document.addEventListener('keydown', function (event) {
-                switch (event.key) {
-                    case ' ':
-                        if (!animationHasRun) {
+        const platform = new Platform();
+
+        let animationHasRun = false;
+
+        platform.draw(ctx);
+        document.addEventListener('keydown', function (event) {
+            switch (event.key) {
+                case ' ':
+                    if (!animationHasRun) {
                         animationHasRun = true;
                         animate();
                     }
-                }
-            });
-            function animate() {
-                if (animationHasRun) {
-                    ctx.clearRect(0, 0, canvas.width, canvas.height);
-                    platform.draw(ctx);
-                    platform.update();
-                }
-                if (platform.frameX !== platform.maxFrame) {
+            }
+        });
+
+        function animate() {
+            if (animationHasRun) {
+                ctx.clearRect(0, 0, canvas.width, canvas.height);
+                platform.draw(ctx);
+                platform.update();
+            }
+            if (platform.frameX !== platform.maxFrame) {
                 setTimeout(function () {
                     requestAnimationFrame(animate);
-                }, 100); 
-                }
-            };
-    }});
-</script>
-Press Space bar to animate
+                }, 100);
+            }
+        };
+    }
+});
+//Press Space bar to animate
